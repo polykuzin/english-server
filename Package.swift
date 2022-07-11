25db6777fe16b272487fe200623684b0dbf84e3a
polykuzin <polykuzin@gmail.com>
2022-07-11 23:45:44 +0300

    removed all redundant

--- Package.swift
+++ Package.swift
@@ -7,14 +7,27 @@ let package = Package(
         .macOS(.v12)
     ],
     dependencies: [
+        
+        /// 🕸 JSON Web Token
+        .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0"),
+        
         /// 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        
+        /// 💻 ORM framework
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+        
+        /// 💽 Postgress driver
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0")
     ],
     targets: [
         .executableTarget(
             name: "english-server",
             dependencies: [
-                .product(name: "Vapor", package: "vapor")
+                .product(name: "JWT", package: "jwt"),
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver")
             ],
             resources: [
                 .copy("menu.json")
--- Sources/english-server/english-server.swift
+++ Sources/english-server/english-server.swift
@@ -12,16 +12,8 @@ public struct english_server {
     
     public static func main() async throws {
         let webapp = Application()
-        let foodTruckServer = FoodTruckServer()
-        try await foodTruckServer.bootstrap()
-        webapp.get("greet", use: Self.greet(_:))
         webapp.post("reflect", use: Self.reflect(_:))
-        webapp.get("donuts", use: foodTruckServer.listOfDonuts(_:))
         try webapp.run()
-    }
-    
-    static func greet(_ req: Request) async throws -> String {
-        return "Hello from Swift Server"
     }
     
     static func reflect(_ req: Request) async throws -> String {
@@ -30,69 +22,4 @@ public struct english_server {
         }
         throw Abort(.badRequest)
     }
-}
-
-struct FoodTruckServer {
-    
-    private let storage = Storage()
-    
-    func bootstrap() async throws {
-        try await self.storage.load()
-    }
-    
-    func listOfDonuts(_ req: Request) async -> [Donut] {
-        return await self.storage.listDonuts()
-    }
-}
-
-actor Storage {
-    let jsonDecoder : JSONDecoder
-    var donuts = [Donut]()
-    
-    init() {
-        self.jsonDecoder = JSONDecoder()
-        self.jsonDecoder.dateDecodingStrategy = .iso8601
-    }
-    
-    func load() throws {
-        guard
-            let path = Bundle.module.path(forResource: "menu", ofType: "json")
-        else {
-            throw Abort(.internalServerError)
-        }
-        guard
-            let data = FileManager.default.contents(atPath: path)
-        else {
-            throw Abort(.noContent)
-        }
-        self.donuts = try self.jsonDecoder.decode([Donut].self, from: data)
-    }
-    
-    func listDonuts() -> [Donut] {
-        return self.donuts
-    }
-}
-
-struct Donut : Encodable, Decodable, Content {
-    public var id : UUID
-    public var name : String
-    public var date : String
-    public var dough : Dough
-    public var glaze : Glaze?
-}
-
-struct Dough : Encodable, Decodable {
-    let name : String
-    let flavors : Flavors
-    let description : String
-}
-
-struct Glaze : Encodable, Decodable {
-    let name : String
-    let description : String
-}
-
-struct Flavors : Encodable, Decodable {
-    let sweet : Int
-    let savory : Int
 }
